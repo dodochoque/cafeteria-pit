dbfcb0905953eead73118b0f752a7c3569fd762f
dodochoque <60689223+dodochoque@users.noreply.github.com>
2025-10-08 13:29:23 -0300

    Delete public directory

--- public/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__.'/../controllers/PedidoController.php';
-$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-if($path==='/adicionar'){ (new PedidoController())->adicionarAoCarrinho(); exit; }
-include __DIR__.'/../views/home.php';
